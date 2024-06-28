42a4a315063161266c99cd96f0a26236205d4f1f
khoinguyen.woay <khoinguyen.woay@gmail.com>
2024-06-28 16:35:53 +0700

    feat: add postgres module

--- libs/postgres/src/lib/postgres.module.ts
+++ libs/postgres/src/lib/postgres.module.ts
@@ -1,8 +1,70 @@
-import { Module } from '@nestjs/common';
+import { Global, Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import {
+  DataSource,
+  EntityManager,
+  EntityTarget,
+  ObjectLiteral,
+  QueryRunner,
+  Repository,
+  SelectQueryBuilder,
+} from 'typeorm';
 
+interface WriteConnection {
+  readonly startTransaction: (
+    level?:
+      | 'READ UNCOMMITTED'
+      | 'READ COMMITTED'
+      | 'REPEATABLE READ'
+      | 'SERIALIZABLE'
+  ) => Promise<void>;
+  readonly commitTransaction: () => Promise<void>;
+  readonly rollbackTransaction: () => Promise<void>;
+  readonly isTransactionActive: boolean;
+  readonly manager: EntityManager;
+}
+
+interface ReadConnection {
+  readonly getRepository: <T extends ObjectLiteral>(
+    target: EntityTarget<T>
+  ) => Repository<T>;
+  readonly query: (query: string) => Promise<any>;
+  readonly createQueryBuilder: <Entity extends ObjectLiteral>(
+    entityClass: EntityTarget<Entity>,
+    alias: string,
+    queryRunner?: QueryRunner
+  ) => SelectQueryBuilder<Entity>;
+}
+
+export let writeConnection = {} as WriteConnection;
+export let readConnection = {} as ReadConnection;
+
+class PostgreSQLService implements OnModuleInit, OnModuleDestroy {
+  private readonly dataSource = new DataSource({
+    type: 'postgres',
+    entities: [],
+    host: environment.POSTGRES_HOST,
+    port: environment.POSTGRES_PORT,
+    database: environment.POSTGRES_DB,
+    username: environment.POSTGRES_USER,
+    password: environment.POSTGRES_PASSWORD,
+    synchronize: true,
+  });
+
+  async onModuleInit(): Promise<void> {
+    await this.dataSource.initialize();
+    if (!this.dataSource.isInitialized)
+      throw new Error('DataSource is not initialized');
+    writeConnection = this.dataSource.createQueryRunner();
+    readConnection = this.dataSource.manager;
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.dataSource.destroy();
+  }
+}
+
+@Global()
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [],
+  providers: [PostgreSQLService],
 })
-export class PostgresModule {}
+export class PostgreSQLModule {}
