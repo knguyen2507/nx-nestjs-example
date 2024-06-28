911feb93242247d7fa1ee9ed811744b0cccbf167
khoinguyen.woay <khoinguyen.woay@gmail.com>
2024-06-28 16:55:14 +0700

    fix: redis module

--- libs/redis/src/lib/redis.module.ts
+++ libs/redis/src/lib/redis.module.ts
@@ -1,8 +1,23 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-ioredis';
+import { environment } from '@libs/utility';
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [
+    CacheModule.registerAsync({
+      useFactory: () => {
+        return {
+          store: redisStore,
+          host: environment.REDIS_HOST,
+          port: environment.REDIS_PORT,
+          username: environment.REDIS_USERNAME,
+          password: environment.REDIS_PASSWORD,
+          no_ready_check: true,
+        };
+      },
+    }),
+  ],
+  exports: [CacheModule],
 })
 export class RedisModule {}
