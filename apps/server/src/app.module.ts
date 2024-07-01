c88305471dda814618c5c87cb0752fc8f1e9a949
khoinguyen.woay <khoinguyen.woay@gmail.com>
2024-07-01 11:12:24 +0700

    fix: postgres module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-
+import { CerbosModule } from '@libs/cerbos';
+import { PostgreSQLModule } from '@libs/postgres';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [CerbosModule, PostgreSQLModule],
   controllers: [AppController],
   providers: [AppService],
 })
